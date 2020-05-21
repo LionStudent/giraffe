1cea04da67103c15e8b26aba18adeed044460cc6
giraffestudent <giraffe.student@yandex.com>
2020-05-21 21:38:52 +0000

    Added and changed files around

--- microservice.py
+++ microservice.py
@@ -56,3 +56,29 @@ def getPosts():
     posts = store.searchTextData(name)
 
     return jsonify(posts)
+    ...
+
+import covid19
+
+import json
+
+...
+
+
+@app.route('/sendSinglePost', methods=['POST'])
+
+def sendSinglePost():
+
+    response = {}
+
+    if request.method == 'POST':
+
+        options = json.loads(request.data)
+
+        filename = covid19.getImageFilename(options)
+
+        options['url'] = store.addImageData(filename)
+
+        store.addTextData(options)
+
+    return jsonify(options)
